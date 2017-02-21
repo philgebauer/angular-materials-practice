3ccb340c483d44df5e9eec157550bfa671ab2e03
PhilGebauer <gebauer.phil@gmail.com>
2017-02-21 11:25:02 -0600

    angular materials working

---
+++ public/scripts/admin.controller.js
@@ -0,0 +1,6 @@
+app.controller('AdminController', ['$http', function($http) {
+  const self = this;
+  console.log("Admin Controller Running");
+
+  
+}]);
--- public/scripts/client-app.js
+++ public/scripts/client-app.js
@@ -1,20 +1,15 @@
 var app = angular.module('app', ['ngMaterial','ngRoute']);
 
-app.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
-  $locationProvider.hashPrefix('');
+app.config(['$routeProvider', function($routeProvider) {
+  // $locationProvider.hashPrefix('');
   $routeProvider
-    .when('/home', {
-      templateUrl: '/views/templates/home.html',
-      controller: 'HomeController',
-      controllerAs: 'home'
-    })
     .when('/admin', {
-      templateUrl: '/views/templates/admin.html',
+      templateUrl: '/views/admin.html',
       controller: 'AdminController',
       controllerAs: 'admin'
     })
     .otherwise({
-      redirectTo: '/home'
+      redirectTo: '/admin'
     });//End route
 
 }]);//End config;
